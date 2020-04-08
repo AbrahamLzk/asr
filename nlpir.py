d1567f73678617f4ed5f2ef7a18c071047f11488
AbrahamLzk <948812511@qq.com>
2020-04-08 20:27:27 +0800

    nlp

--- nlpir.py
+++ nlpir.py
@@ -65,25 +65,35 @@ class DFAFilter():
 
 
 if __name__ == '__main__':
-    pynlpir.open()  # 打开分词器
-    f = open(str(sys.argv[1])+'\\result.txt', mode='r', encoding='utf-8')
-    text = f.read()
-    f.close()
-    #print(pynlpir.segment(text, pos_tagging=False))
-    with open(str(sys.argv[1])+'\\words.txt', "a", encoding='utf-8') as f:
-        for t in pynlpir.segment(text, pos_tagging=False):
-            if str(t) != '，' and str(t) != '。' and str(t) != '？':
-                f.write(str(t))
-                f.write(str(','))
-        f.close()
-    pynlpir.close()
-    gfw = DFAFilter()
-    path=os.getcwd()+"\\sex.txt"
-    gfw.parse(path)
-    f = open(str(sys.argv[1])+'\\words.txt', mode='r', encoding='utf-8')
-    text = f.read()
-    f.close()
-    result = gfw.filter(text)
-    print(result)
-    time2 = time.time()
-    print('总共耗时：' + str(time2 - time1) + 's')
+    with open(p+'\\time_data.txt', "r", encoding='utf-8') as file:
+        temp = file.readline()
+        print(temp)
+        file.close()
+    f = open(str(sys.argv[1])+'\\result1.txt', mode='r', encoding='utf-8')
+    for i in range(temp):
+        text = f.readline()
+        if text != '':
+            pynlpir.open()  # 打开分词器
+            #print(pynlpir.segment(text, pos_tagging=False))
+            with open(str(sys.argv[1])+'\\words.txt', "a", encoding='utf-8') as f:
+                for t in pynlpir.segment(text, pos_tagging=False):
+                    if str(t) != '，' and str(t) != '。' and str(t) != '？':
+                        f.write(str(t))
+                        f.write(str(','))
+                f.close()
+            sentence = ''
+            for t in pynlpir.segment(text, pos_tagging=False):
+                if str(t) != '，' and str(t) != '。' and str(t) != '？':
+                    sentence += str(t)
+                    sentence += ','
+            pynlpir.close()
+            gfw = DFAFilter()
+            sensitive = ['色情','反动']
+            for sen in sensitive:
+                path=os.getcwd()+'\\' +sen+ '.txt'
+                gfw.parse(path)
+                result = gfw.filter(sentence)
+                print(result)
+                print('\n'+sen+'敏感词'+'，在第%s句'%(i,))
+            time2 = time.time()
+            print('总共耗时：' + str(time2 - time1) + 's')
